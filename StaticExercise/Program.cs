4980b1280415e79aa7e4ca59af4930b3a3166f05
Gashaye <goshu.anley@dakota-consulting.com>
2021-03-29 21:25:25 -0400

    First Commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"10 Fahrenheit = {TempConverter.FahrenheitToCelsius(10)} celcius");
+
+           Console.WriteLine($"50 Celcius = {TempConverter.CelsiusToFahrenheit(50)} Fahrenheit");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double celsius)
+        {
+            double fahrenheit;
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            return fahrenheit;
+        }
+
+        public static double CelsiusToFahrenheit(double fahrenheit)
+        {
+            double celsius;
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}
